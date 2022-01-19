0765c641364e44e93c0c25af1a037b77ae0d508f
Bora Yüret <borayuret@gmail.com>
2022-01-19 11:32:10 +0300

    Initial commit

---
+++ src/test/java/com/bilgeadam/boosteurekaserver/BoostEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.boosteurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
